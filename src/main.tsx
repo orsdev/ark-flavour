e63204f77dfdce5da3c6a1308796e74ee121c326
ogundare samuel <oluwadareysamuel@gmail.com>
2023-06-03 19:18:08 +0100

    feat: setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 
 import "./styles/global.css";
 
+// Create a client
+export const query_client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchInterval: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <QueryClientProvider client={query_client}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
