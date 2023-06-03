6d2ebee0804e6b7c317f81d6015cd1f089d5464c
ogundare samuel <oluwadareysamuel@gmail.com>
2023-06-03 22:48:46 +0100

    feat: minor css update

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -6,12 +6,12 @@ interface DefaultLayoutProps {
 }
 
 export const DefaultLayout: FC<DefaultLayoutProps> = ({ children }) => (
-  <>
+  <div className="relative h-full min-h-screen flex flex-col">
     <Navbar />
-    <main>{children}</main>
+    <main className="flex-1">{children}</main>
     <footer className="w-full bg-yellow-50 h-[60px] flex items-center mt-12 py-2 justify-center flex-col pt-3">
       <img className="w-[30px]" src="/images/logo.png" alt="logo" />
       <h3 className="text-lg text-red-500 cursive mt-1">Ark's Flavour</h3>
     </footer>
-  </>
+  </div>
 );
