cd91442b0d1c273e8c274edd94cf16ee3b9bed80
ogundare samuel <oluwadareysamuel@gmail.com>
2023-06-03 20:43:04 +0100

    feat: add loading card component

--- src/components/home/random/Random.tsx
+++ src/components/home/random/Random.tsx
@@ -1,26 +1,29 @@
-import { RecipeCard } from "@/components/common";
+import { LoadingCard, RecipeCard } from "@/components/common";
 import { useRandomRecipes } from "@/hooks";
 import { RecipeProps } from "@/hooks/type";
 
 export const Random = () => {
-  const { randomRecipes } = useRandomRecipes();
+  const { randomRecipes, isLoading } = useRandomRecipes();
 
   return (
-    <div className="mt-[100px] max-w-[900px] mx-auto px-[20px] lg:px-0">
-      <h1 className="mb-32 text-center text-6xl cursive text-gray-600">
-        Discover Random Recipes
-      </h1>
-      <div className="grid grid-cols-[repeat(auto-fit,_minmax(240px,_1fr))] gap-[20px] gap-y-[40px]">
-        {randomRecipes &&
-          randomRecipes?.map((recipe: RecipeProps) => (
-            <RecipeCard
-              key={recipe.id}
-              name={recipe.title}
-              image={recipe.image}
-              summary={recipe.summary}
-            />
-          ))}
+    <>
+      <div className="mt-[100px] max-w-[900px] mx-auto px-[20px] lg:px-0">
+        <h1 className="mb-32 text-center text-6xl cursive text-gray-600">
+          Discover Random Recipes
+        </h1>
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(240px,_1fr))] gap-[20px] gap-y-[40px]">
+          {isLoading && <LoadingCard />}
+          {randomRecipes &&
+            randomRecipes?.map((recipe: RecipeProps) => (
+              <RecipeCard
+                key={recipe.id}
+                name={recipe.title}
+                image={recipe.image || "/images/no-image.svg"}
+                summary={recipe.summary}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
