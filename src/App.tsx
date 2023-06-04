e55ba40e9f76a88068ea919294bc18d1e6c0ca98
ogundare samuel <oluwadareysamuel@gmail.com>
2023-06-04 08:30:05 +0100

    feat: add recipes route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
 import Details from "@/pages/Details";
-import NotFound from "./pages/NotFound";
+import NotFound from "@/pages/NotFound";
+import Recipes from "@/pages/Recipes";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} />
       <Route path="/details/:id" element={<Details />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
