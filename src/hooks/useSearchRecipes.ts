87c377e82fb44a252738a1ae8e7335c6c1def45b
ogundare samuel <oluwadareysamuel@gmail.com>
2023-06-04 08:31:18 +0100

    feat: route to 404 page when recipes are not found

--- src/hooks/useSearchRecipes.ts
+++ src/hooks/useSearchRecipes.ts
@@ -1,5 +1,6 @@
 import { fetcher } from "@/config";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 type UseSearchRecipesProps = {
   page: number,
@@ -10,13 +11,18 @@ type UseSearchRecipesProps = {
 export const PerPage = 14;
 
 export const useSearchRecipes = ({ page, ingredients, diet }: UseSearchRecipesProps) => {
+  const navigate = useNavigate();
+
   const { data, isLoading } = useQuery({
     queryKey: ["search-recipes", { ingredients, diet, page }],
     queryFn: () => fetcher({
       url: `/complexSearch?includeIngredients=${ingredients}&diet=${diet}&offset=${(page - 1) * PerPage}&number=${PerPage}`,
     }),
     retry: false,
-    staleTime: Infinity
+    staleTime: Infinity,
+    onError() {
+      navigate('/404')
+    }
   });
 
   return {
